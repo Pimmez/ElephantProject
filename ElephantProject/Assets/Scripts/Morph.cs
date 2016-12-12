2f7ae753b12e5d8d1b96b0252ca106fd27333a73
Tim <tbajmat@gmail.com>
2016-12-12 13:17:50 +0100

    Input + Morphing

---
+++ ElephantProject/Assets/Scripts/Morph.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Morph : MonoBehaviour {
+
+	public GameObject FirstAnimal;
+	public GameObject SecAnimal;
+	private int count = 1;
+
+	//VERWIJDER PLZ NA DEMO
+	public Text text;
+
+	void Update()
+	{
+		if(InputManager.BButton())
+			count++;
+
+		if(count > 2)
+			count = 1;
+
+		ChangeAnimal();
+	}
+
+	void ChangeAnimal()
+	{
+		switch (count)
+		{
+		case 1:
+			FirstAnimal.SetActive(true);
+			SecAnimal.SetActive(false);
+			text.text = "Olifant";
+			break;
+		case 2:
+			FirstAnimal.SetActive(false);
+			SecAnimal.SetActive(true);
+			text.text = "Ander Dier";
+			break;
+		}
+	}
+}
